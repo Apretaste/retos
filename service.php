fdd43cf3967cd24dff1b30e2745e2d6c170e9767
kumahacker <kumahavana@gmail.com>
2017-11-17 00:32:38 -0500

    fix chat link

--- service.php
+++ service.php
@@ -297,7 +297,7 @@ class Retos extends Service
 					],
 					4 => [
 						'caption' => 'Enviar una nota privada a otro usuario',
-						'link' => 'CHAT @amigo Hola que tal..',
+						'link' => 'CHAT',
 						'checker' => [
 							'type' => 'count',
 							'data' => "SELECT count(*) as total FROM _note WHERE from_user = '{$request->email}' AND week('{$this->now}') = week(send_date) AND year(send_date) = year('{$this->now}')"
