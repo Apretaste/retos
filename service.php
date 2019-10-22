14fb7b3223d37e2c0d40dd053c29f13678110811
kumahacker <kumahavana@gmail.com>
2019-10-22 23:32:06 +0200

    new retos

--- service.php
+++ service.php
@@ -5,59 +5,33 @@ class Service
 	/**
 	 * Display the daily challenge
 	 *
-	 * @author salvipascual
 	 * @param Request  $request
 	 * @param Response $response
+	 *
+	 * @author salvipascual
 	 */
-	public function _main (Request $request, Response $response)
+	public function _main(Request $request, Response $response)
 	{
-		$content = [
-			"icon" => "chrome_reader_mode",
-			"title" => "Leer los Términos de uso",
-			"desc" => "Nuestros términos de uso le muestra reglas básicas de uso que cada usuario de Apretaste debe cumplir.",
-			"credits" => "0.2",
-			"link" => "TERMINOS",
-		];
+		$content = Challenges::getCurrent($request->person->id);
 
 		// send data to the view
 		$response->setTemplate('open.ejs', $content);
-//		$response->setTemplate('closed.ejs', $content);
 	}
 
 	/**
 	 * Display the challenges completed
 	 *
-	 * @author salvipascual
 	 * @param Request  $request
 	 * @param Response $response
+	 *
+	 * @author salvipascual
 	 */
-	public function _done (Request $request, Response $response)
+	public function _done(Request $request, Response $response)
 	{
 		$content = [
-			"total" => 3.5,
-			"challenges" => []
+			"total"      => Challenges::earned($request->person->id),
+			"challenges" => Challenges::history($request->person->id)
 		];
-
-		$challenge = new stdClass();
-		$challenge->icon = "ac_unit";
-		$challenge->title = "Completar su perfil de la app y poner foto";
-		$challenge->completed = "09/28/2019";
-		$challenge->credits = 2;
-		$content["challenges"][] = $challenge;
-
-		$challenge = new stdClass();
-		$challenge->icon = "all_inclusive";
-		$challenge->title = "Decirnos donde escuchó sobre la app";
-		$challenge->completed = "09/22/2019";
-		$challenge->credits = 1;
-		$content["challenges"][] = $challenge;
-
-		$challenge = new stdClass();
-		$challenge->icon = "beach_access";
-		$challenge->title = "Leer como ganar crédito desde el servicio Crédito";
-		$challenge->completed = "09/21/2019";
-		$challenge->credits = 1.5;
-		$content["challenges"][] = $challenge;
 
 		// send data to the view
 		$response->setTemplate('done.ejs', $content);
@@ -66,19 +40,26 @@ class Service
 	/**
 	 * Skips the current challenge and charges the user
 	 *
-	 * @author salvipascual
 	 * @param Request  $request
 	 * @param Response $response
+	 *
+	 * @throws \Exception
+	 * @author salvipascual
 	 */
-	public function _skip (Request $request, Response $response)
+	public function _skip(Request $request, Response $response)
 	{
+		$result = Challenges::jump($request->person);
 		// if user do not have enough credits
-		if(true) {
-			return $response->setTemplate('message.ejs', [
+		if ($result===false) {
+			$response->setTemplate('message.ejs', [
 				'header' => 'No tiene créditos',
-				'icon' => 'sentiment_very_dissatisfied',
-				'text' => 'Usted no tiene §0.2 de crédito que cuesta saltar un reto, por lo cual no pudimos continuar.',
+				'icon'   => 'sentiment_very_dissatisfied',
+				'text'   => 'Usted no tiene §0.2 de crédito que cuesta saltar un reto, por lo cual no pudimos continuar.',
 			]);
+
+			return;
 		}
+
+		$this->_main($request, $response);
 	}
 }
